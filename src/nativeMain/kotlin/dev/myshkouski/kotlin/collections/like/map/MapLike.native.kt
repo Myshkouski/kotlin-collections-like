b5b26b5e931065d482c17f924455c82ac8fc80e1
Alexei <alexeimyshkouski@gmail.com>
2025-01-12 22:34:13 +0300

    feat: implement MapLike native

--- src/nativeMain/kotlin/dev/myshkouski/kotlin/collections/like/map/MapLike.native.kt
+++ src/nativeMain/kotlin/dev/myshkouski/kotlin/collections/like/map/MapLike.native.kt
@@ -2,12 +2,22 @@ package dev.myshkouski.kotlin.collections.like.map
 
 import dev.myshkouski.kotlin.collections.like.iterator.toIterable
 
-actual fun <K, V> mapLikeOf(): MapLike<K, V> = DefaultMapLikeImpl()
+actual fun <K, V> mapLikeOf(): MapLike<K, V> {
+    return DefaultMapLikeImpl()
+}
 
-actual fun <K, V> mapLikeOf(vararg pairs: Pair<K, V>): MapLike<K, V> = DefaultMapLikeImpl(*pairs)
+actual fun <K, V> mapLikeOf(vararg pairs: Pair<K, V>): MapLike<K, V> {
+    return DefaultMapLikeImpl(*pairs)
+}
 
-actual fun <K, V> mapLikeOf(vararg pairs: PairLike<K, V>): MapLike<K, V> = DefaultMapLikeImpl(*pairs.toPairs())
+actual fun <K, V> mapLikeOf(vararg pairs: PairLike<K, V>): MapLike<K, V> {
+    return mapLikeOf(*pairs.toPairs())
+}
 
-actual fun <K, V> MapLike<K, V>.toMapLike(): MapLike<K, V> = DefaultMapLikeImpl(*entries.toIterable().map { it.toPair() }.toTypedArray())
+actual fun <K, V> MapLike<K, V>.toMapLike(): MapLike<K, V> {
+    return mapLikeOf(*entries.toIterable().toList().toTypedArray())
+}
 
-private fun <K, V> Array<out PairLike<K, V>>.toPairs(): Array<Pair<K, V>> = map { it.toPair() }.toTypedArray()
+internal fun <K, V> Array<out PairLike<K, V>>.toPairs(): Array<Pair<K, V>> {
+    return map { it.toPair() }.toTypedArray()
+}
--- src/nativeMain/kotlin/dev/myshkouski/kotlin/collections/like/map/MutableMapLike.native.kt
+++ src/nativeMain/kotlin/dev/myshkouski/kotlin/collections/like/map/MutableMapLike.native.kt
@@ -1,17 +1,19 @@
 package dev.myshkouski.kotlin.collections.like.map
 
-actual fun <K, V> mutableMapLikeOf(vararg pairs: PairLike<K, V>): MutableMapLike<K, V> {
-    TODO("Not yet implemented")
+import dev.myshkouski.kotlin.collections.like.iterator.toIterable
+
+actual fun <K, V> mutableMapLikeOf(): MutableMapLike<K, V> {
+    return DefaultMutableMapLikeImpl()
 }
 
 actual fun <K, V> mutableMapLikeOf(vararg pairs: Pair<K, V>): MutableMapLike<K, V> {
-    TODO("Not yet implemented")
+    return DefaultMutableMapLikeImpl(*pairs)
+}
+
+actual fun <K, V> mutableMapLikeOf(vararg pairs: PairLike<K, V>): MutableMapLike<K, V> {
+    return mutableMapLikeOf(*pairs.toPairs())
 }
 
 actual fun <K, V> MapLike<K, V>.toMutableMapLike(): MutableMapLike<K, V> {
-    TODO("Not yet implemented")
-}
-
-actual fun <K, V> mutableMapLikeOf(): MutableMapLike<K, V> {
-    TODO("Not yet implemented")
+    return mutableMapLikeOf(*entries.toIterable().toList().toTypedArray())
 }
